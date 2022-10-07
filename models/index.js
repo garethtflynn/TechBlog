b6e993ab6cd4b04a33d24c78ac72c6d1d1dd39ae
Gareth Flynn <gareth.t.flynn@gmail.com>
2022-10-07 15:19:18 -0400

    forgot to add relationships of models

--- models/index.js
+++ models/index.js
@@ -1,7 +1,31 @@
 const User = require('./User');
 
+User.hasMany(Post, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+})
 
+User.hasMany(Comment, {
+    foreignKey: 'uder_id',
+    onDelete: 'CASCADE'
+})
 
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+})
 
+Post.hasMany(Comment, {
+    foreignKey: 'post_id',
+    onDelete: 'CASCADE'
+})
+
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+})
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+})
 
 module.exports = { User,  };
